f06093a819c32495f9c49b3a39030c61f68f054d
Jesus Sierralaya <jesus.sierralaya@hotmail.com>
2024-08-15 23:48:03 +0200

    Adds docs

---
+++ docs/assets/db-data.ts
@@ -0,0 +1,20 @@
+export const CHESTER_VERSIONS: any = [
+    {
+        id: 1,
+        description: "Desert Chester",
+        iconUrl: 'assets/desert_cat-min.png',
+        longDescription: "Chester is in the desert under a full moon."
+    },
+    {
+        id: 2,
+        description: "Astro Chester",
+        iconUrl: 'assets/astro_chester.webp',
+        longDescription: "Chester is in space on a mission."
+    },
+    {
+        id: 3,
+        description: "Tech Chester",
+        iconUrl: 'assets/tech_chester.webp',
+        longDescription: "Chester has many tech parts; it is a cat from the future."
+    },
+]
