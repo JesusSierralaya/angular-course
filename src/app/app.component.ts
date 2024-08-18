38bf0c572a4ba7854d3061a9117048632588dac1
Jesus Sierralaya <jesus.sierralaya@hotmail.com>
2024-08-18 02:41:47 +0200

    adds use of pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,30 @@ import { RouterOutlet } from '@angular/router';
 import { CatVersionCardComponent } from './verse-card/verse-card.component';
 import { CHESTER_VERSIONS } from '../assets/db-data';
 import { CatVersions } from './interfaces/cat-versions';
+import {
+  DatePipe,
+  UpperCasePipe,
+  TitleCasePipe,
+  DecimalPipe,
+  CurrencyPipe,
+  PercentPipe,
+  JsonPipe
+} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CatVersionCardComponent],
+  imports: [
+    RouterOutlet,
+    CatVersionCardComponent,
+    DatePipe,
+    UpperCasePipe,
+    TitleCasePipe,
+    DecimalPipe,
+    CurrencyPipe,
+    PercentPipe,
+    JsonPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -39,10 +58,20 @@ export class AppComponent {
   // Chester versions for cards
 
   // chesterVersions = CHESTER_VERSIONS
-  // chesterVersions: CatVersions[] = [] 
-  chesterVersions: CatVersions[] = [...CHESTER_VERSIONS]
+  // chesterVersions: CatVersions[] = []
+  chesterVersions: CatVersions[] = [...CHESTER_VERSIONS];
 
   onVersionSelected(version: CatVersions) {
     console.log('App component - click event ', version);
   }
+
+  startDate = new Date(2000, 0, 1);
+
+  someText = 'some text transformed to uppercase';
+
+  someText2 = 'some text transformed to titlecase';
+
+  price = 3.99;
+
+  version = CHESTER_VERSIONS[0]
 }
